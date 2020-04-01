6af3368dc3584384eb1b91c7207b53245028ead1
wf <1244353765@qq.com>
2020-04-01 23:30:54 +0800

    day08

---
+++ 语法基础/day08/列表定义和遍历.py
@@ -0,0 +1,26 @@
+"""
+列表定义和遍历
+
+"""
+
+list1 = [1, 3, 5, 7, 100]
+print(list1) # [1, 3, 5, 7, 100]
+list2 = ['hello'] * 3
+print(list2) # ['hello','hello','hello']
+print(len(list1)) # 5
+print(list1[0]) # 1
+print(list1[4]) # 100
+print(list1[-1]) # 100
+print(list1[-3]) # 5
+list1[2] = 300
+print(list1) # [1, 3, 300, 7, 100]
+# 通过循环用下标遍历
+for index in range(len(list1)):
+    print(list1[index])
+# 通过for循环遍历
+for elem in list1:
+    print(elem)
+# 通过enumerate函数处理列表之后再遍历可以同时获得元素索引和值
+for index, elem in enumerate(list1):
+    print(index, elem)
+
