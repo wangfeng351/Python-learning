9da17ca232942bd2cff09ff3972f33d7f9d2bc97
wf <1244353765@qq.com>
2020-04-14 00:06:04 +0800

    day20

---
+++ 语法基础/day20/爬取实验楼.py
@@ -0,0 +1,30 @@
+
+import requests
+from lxml import html
+
+def crawl():
+    course_list = []
+    for num in range(1, 25):
+        url = 'https://www.shiyanlou.com/courses/?page=' + str(num)
+        content = requests.get(url)
+        tree = html.fromstring(content.text)
+        course_names = tree.xpath('//h6[@class="course-name"]/text()')
+        course_descriptions = tree.xpath(
+            '//div[@class="course-description"]/text()')
+            course_covers = tree.xpath('//img[@class="cover-image"]/@src')
+            for index in range(0, len(course_names) - 1)
+                temp_dict = {}
+                temp_dict['name'] = course_names[index].strip()
+                temp_dict['description'] = course_descriptions[index].strip()
+                temp_dict['cover'] = course_covers[index]
+                course_list.append(temp_dict)
+                # print(len(course_list))
+        return course_list
+
+
+if __name__ == '__main__':
+    list = crawl()
+    print(len(list))
+    # print(list)
+
+# 运行结果为： 456门课
