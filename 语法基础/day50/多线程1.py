fca3abe865ed80da293fa7b62944e921e15a381d
wf <1244353765@qq.com>
2020-05-14 01:10:29 +0800

    day50

---
+++ 语法基础/day50/多线程1.py
@@ -0,0 +1,37 @@
+"""
+多线程1
+"""
+
+import threading
+import time
+
+# 创建一个线程
+my_thread = threading.Thread()
+# 创建一个名为 my_thread的线程
+my_thread = threading.Thread(name='my_thread')
+
+
+def print_i(i):
+    print('打印i:%d' % (i,))
+
+
+# 通过参数target传入，参数类型为callable
+my_thread = threading.Thread(target=print_i, args=(1,))
+# 启动线程
+my_thread.start()
+
+
+def print_time():
+    # 在每个线程中打印5次
+    for _ in range(5):
+        # 模拟打印前的相关处理逻辑耗时
+        time.sleep(0.1)
+        print('当前线程%s，打印结束时间为：%s' %
+              (threading.current_thread().getName(), time.time()))
+
+
+# 开辟三个线程，装载到threads中
+threads = [threading.Thread(name='t%d' % (
+    i,), target=print_time) for i in range(3)]
+# 启动三个线程
+[t.start() for t in threads]
