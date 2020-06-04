175ac9734f2ec61d798b7d2cb2782eddba53c98b
wf <1244353765@qq.com>
2020-06-04 23:16:28 +0800

    day71

--- 语法基础/day71/Pandas.py
+++ 语法基础/day71/Pandas.py
@@ -101,3 +101,45 @@ grid = (
     .add(bar, grid_opts=opts.GridOpts(pos_left="30%"))
 )
 grid.render(path='./语法基础/res/html/movie1.html')
+
+c = (
+    Pie()
+    .add(
+        "",
+        [list(z) for z in top10],
+        radius=["40%", "55%"],
+        label_opts=opts.LabelOpts(
+            position="outside",
+            formatter="{a|{a}}{abg|}\n{hr|}\n {b|{b}: }{c} {per|{d}%} ",
+            background_color='#eee',
+            border_color="#aaa",
+            border_width=1,
+            border_radius=4,
+            rich={
+                "a": {"color": "#999", "lineHeight": 22, "align": "center"},
+                "abg": {
+                    "backgroundColor": "#e3e3e3",
+                    "width": "100%",
+                    "align": "right",
+                    "height": 22,
+                    "borderRadius": [4, 4, 0, 0],
+                },
+                "hr": {
+                    "borderColor": "#aaa",
+                    "width": "100%",
+                    "borderWidth": 0.5,
+                    "height": 0,
+                },
+                "b": {"fontSize": 16, "lineHeight": 33},
+                "per": {
+                    "color": "#eee",
+                    "backgroundColor": "#334455",
+                    "padding": [2, 4],
+                    "borderRadius": 2,
+                }
+            }
+
+        )
+    )
+)
+c.render(path='./语法基础/res/html/movie2.html')
